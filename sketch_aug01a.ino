71f8494f2f41fd123d22a0a492d0882449f2b43f
haziq danial <haziqdanial031@gmail.com>
2025-08-04 10:32:32 +0800

    refactoring system error checking

--- sketch_aug01a.ino
+++ sketch_aug01a.ino
@@ -1,6 +1,7 @@
 #include "global.h"
 
-#define LED_DEBUG_PIN   14
+#define LED_DEBUG_PIN   13
+#define SYS_Error_Check(x)  if(( SYS_ERROR_NUM = (x)) != 0) SYS_Fatal_Error(SYS_ERROR_NUM)
 
 int32_t SYS_ERROR_NUM;
 
@@ -14,15 +15,13 @@ void SYS_Fatal_Error(int32_t err) {
 void SYS_Init(void) {
   Serial.begin(9600);
 
+  SYS_Error_Check(GPIO_Init());
+  SYS_Error_Check(GPIO_Write(LED_DEBUG_PIN, GPIO_OUTPUT));
+  SYS_Error_Check(GPIO_Mode(LED_DEBUG_PIN, GPIO_OUTPUT));
+
   SYS_ERROR_NUM = GPIO_Init();
   if(SYS_ERROR_NUM != GPIO_SUCCESS) SYS_Fatal_Error(SYS_ERROR_NUM);
 
-  // if(GPIO_Init() != GPIO_SUCCESS) SYS_Fatal_Error();
-
-  if((SYS_ERROR_NUM = GPIO_Write(LED_DEBUG_PIN, GPIO_OUTPUT)) != GPIO_SUCCESS) SYS_Fatal_Error(SYS_ERROR_NUM);
-  if((SYS_ERROR_NUM = GPIO_Mode(LED_DEBUG_PIN, GPIO_OUTPUT)) != GPIO_SUCCESS) SYS_Fatal_Error(SYS_ERROR_NUM);
-  // GPIO_Init();
-  // GPIO_Mode(LED_DEBUG_PIN, GPIO_OUTPUT);
 }
 
 
